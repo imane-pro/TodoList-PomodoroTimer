fc2e217b16feca213345bc1129ce916090abade1
imane-pro <imanearroub67@gmail.com>
2024-09-18 20:53:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import TicTacToe from './TicTacToe'
+
+
+const App = () => {
+  return (
+    <div className="app-container">
+    <TicTacToe/>
+    </div>
+  );
+};
+
+export default App;
